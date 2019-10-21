1338b3873efde7492ea40936814e8675fb5e6aaf
wang_unicorn <unicorn_zhi@163.com>
2019-10-21 14:23:20 +0800

    v0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("1");
+class FastCache{
+
+    constructor(){
+        this.list = {};
+    }
+
+    set(key,value){
+        this.list[key]=value;
+    }
+
+    get(key){
+        return this.list[key];
+    }
+}
+
+window.FastCache = FastCache;
